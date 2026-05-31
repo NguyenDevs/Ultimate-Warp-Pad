f426d6512683099334cc55c593b405d73977c572
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-05-31 22:05:40 +0700

    chore: remove build/.gradle/.idea from git tracking; fix sound order

--- src/main/java/org/nguyendevs/ultimateWarpPad/gui/WarpSelectionGUI.java
+++ src/main/java/org/nguyendevs/ultimateWarpPad/gui/WarpSelectionGUI.java
@@ -331,8 +331,10 @@ public class WarpSelectionGUI {
         }
 
         Location sourceLoc = source.getLocation();
-        sourceLoc.getWorld().playSound(sourceLoc, "minecraft:block.beacon.power_select", SoundCategory.AMBIENT, 1.0f, 0.8f);
-        sourceLoc.getWorld().playSound(sourceLoc, "minecraft:block.beacon.activate", SoundCategory.AMBIENT, 1.0f, 1.0f);
+        sourceLoc.getWorld().playSound(sourceLoc, "minecraft:block.beacon.activate", SoundCategory.AMBIENT, 1.0f, 0.5f);
+        sourceLoc.getWorld().playSound(sourceLoc, "minecraft:block.beacon.power_select", SoundCategory.AMBIENT, 0.5f, 0.8f);
+        sourceLoc.getWorld().playSound(sourceLoc, "minecraft:ambient.basalt_deltas.loop", SoundCategory.AMBIENT, 1.0f, 0.5f);
+        sourceLoc.getWorld().playSound(sourceLoc, "minecraft:ambient.soul_sand_valley.mood", SoundCategory.AMBIENT, 1.0f, 0.5f);
 
         animationManager.playAnimation(source);
 
